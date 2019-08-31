3d147637a2eceb1ad348557c4b40ce1ee3bdc612
sriharsha <harsha.avanchi@gmail.com>
2019-08-31 08:46:51 +0530

    Changed pom.xml

--- src/test/java/com/AutomationTalks/demoProject1/Test2.java
+++ src/test/java/com/AutomationTalks/demoProject1/Test2.java
@@ -1,11 +1,8 @@
 package com.AutomationTalks.demoProject1;
 
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class Test2 {
